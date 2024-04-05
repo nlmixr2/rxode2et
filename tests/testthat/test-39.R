2d4f4454b6fac3fe3380a9c547c8abf4aa7553ca
Matthew L. Fidler <matthew.fidler@gmail.com>
2024-04-05 18:14:51 -0500

    Be careful with NA times

--- tests/testthat/test-39.R
+++ tests/testthat/test-39.R
@@ -2,7 +2,7 @@ test_that("test import which was issue #39", {
 
   e <- et()
 
-  e$importEventTable(data.frame(
+  expect_warning(e$importEventTable(data.frame(
     ID = "A",
     TIME = c(738.9333333, NA),
     CMT = c("depot", "central"),
@@ -10,7 +10,7 @@ test_that("test import which was issue #39", {
     EVID = c(1, 0),
     DOSE = 300,
     COVAR = 1
-  ))
+  )))
 
 
-})
+, })
